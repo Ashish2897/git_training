e4181cf69aafb4d25e0268d135153fee82472a53
Ashish2897 <id>
2022-08-10 12:24:39 +0530

    Calculator.java commit

---
+++ Testing/src/testing/Calculator.java
@@ -0,0 +1,18 @@
+package testing;
+
+public class Calculator {
+
+//	public int fact(int a) {
+//		int f=1;
+//		for(int i=1;i<=a;i++)
+//		{
+//			f=f*i;
+//		}
+//		return f;
+//		
+	public int add(int a,int b)
+	{
+		int c = a + b;
+		return c;
+	}
+}
